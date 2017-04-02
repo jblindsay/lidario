c95055b66492324f8bff89796a6884415987bc44
John Lindsay <jlindsay@uoguelph.ca>
2017-04-02 14:13:21 -0400

    Initial commit
    Initial commit

---
+++ lidario_test.go
@@ -0,0 +1,118 @@
+package lidario
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestReadLasFile(t *testing.T) {
+	// fileName := "/Users/johnlindsay/Documents/data/GarveyGlenWatershed/RGB_4_529_150502_1812__0_178080_2784.las"
+	fileName := "testdata/sample.las"
+	var lf *LasFile
+	var err error
+	lf, err = NewLasFile(fileName, "r")
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+	defer lf.Close()
+	fmt.Printf("%v\n", lf.Header)
+
+	// Print the VLR data
+	fmt.Println("VLRs:")
+	for _, vlr := range lf.VlrData {
+		fmt.Println(vlr)
+	}
+
+	j := 1000000
+
+	x, y, z, err := lf.GetXYZ(j)
+	fmt.Printf("Point %v: (%f, %f, %f) Error: %v\n", j, x, y, z, err)
+	var p LasPointer
+
+	p, err = lf.LasPoint(j)
+	if err != nil {
+		fmt.Println(err)
+		t.Fatal()
+	}
+	fmt.Println("Point format:", p.Format())
+
+	oldProgress := -1
+	progress := 0
+	for i := 0; i < int(lf.Header.NumberPoints); i++ {
+		if p, err := lf.LasPoint(i); err != nil {
+			fmt.Println(err)
+			t.Fatal()
+		} else {
+			if i < 10 {
+				pd := p.PointData()
+				fmt.Printf("Point %v: (%f, %f, %f, %v, %v, %f)\n", i, pd.X, pd.Y, pd.X, pd.Intensity, pd.ClassBitField.ClassificationString(), p.GpsTimeData())
+			}
+			progress = int(100.0 * float64(i) / float64(lf.Header.NumberPoints))
+			if progress != oldProgress {
+				oldProgress = progress
+				if progress%10 == 0 {
+					fmt.Printf("Progress: %v\n", progress)
+				}
+			}
+		}
+	}
+
+	// for i := j; i < j+100; i++ {
+	// 	if p, err = lf.LasPoint(i); err != nil {
+	// 		fmt.Println(err)
+	// 		t.Fatal()
+	// 	}
+	// 	pd := p.PointData()
+	// 	fmt.Printf("Point %v: (%f, %f, %f, %v, %v, %f)\n", i, pd.X, pd.Y, pd.X, pd.Intensity, pd.ClassBitField.ClassificationString(), p.GpsTimeData())
+	// 	// cbf := p.GetClassBitField()
+	// 	// fmt.Printf("Point %v: (%f, %f, %f, %v, %v)\n", i, p.GetX(), p.GetY(), p.GetZ(), p.GetIntensity(), cbf.ClassificationString())
+	// }
+
+}
+
+func TestWriteLasFile(t *testing.T) {
+	fileName := "testdata/sample.las"
+	var lf *LasFile
+	var err error
+	lf, err = NewLasFile(fileName, "r")
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+	defer lf.Close()
+
+	// fmt.Println(lf.Header.VersionMinor)
+
+	newFileName := "testdata/newFile.las"
+	newLf, err := InitializeUsingFile(newFileName, lf)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+	defer newLf.Close()
+
+	// fmt.Println(lf.Header.VersionMinor)
+	// fmt.Println(newLf.Header.VersionMinor)
+
+	progress := 0
+	oldProgress := -1
+	for i := 0; i < int(lf.Header.NumberPoints); i++ {
+		if p, err := lf.LasPoint(i); err != nil {
+			fmt.Println(err)
+			t.Fatal()
+		} else {
+			if p.PointData().Z < 100.0 {
+				newLf.AddLasPoint(p)
+			}
+		}
+		progress = int(100.0 * float64(i) / float64(lf.Header.NumberPoints))
+		if progress != oldProgress {
+			oldProgress = progress
+			if progress%10 == 0 {
+				fmt.Printf("Progress: %v\n", progress)
+			}
+		}
+	}
+
+}
